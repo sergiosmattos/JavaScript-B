d3447bfebf242999a69a4a75796196f4ba7dcb5a
sergiosmattos <sergio.lsmattos@gmail.com>
2025-07-01 12:25:25 -0300

    Aula de JavaScript 01/07

---
+++ javascript/arquivo.js
@@ -0,0 +1,33 @@
+var global = "Variavel Global";
+console.log(global);
+
+function saudacao() {
+
+    alert("SIM! SOU o bem-vindo 3! seja bem vindo ao nosso site 3");
+
+}
+
+function exemplo2() {
+
+    var local = "Variavel Local"
+    console.log(local);
+
+}
+
+exemplo2();
+// console.log(local);
+
+let x = 10;
+if ( x > 5 ) {
+
+    let y = 20;
+    console.log("valor de y: ", y);
+
+}
+
+console.log(x);
+// console.log(y);
+
+const _constante = "texto";
+
+console.log(_constante);
