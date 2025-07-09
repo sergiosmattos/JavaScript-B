5f7053f0da68d670919f698020ff52ea41773c43
sergiosmattos <sergio.lsmattos@gmail.com>
2025-07-09 18:55:34 -0300

    09/07 Questões 3 - 7

--- exercicios/script.js
+++ exercicios/script.js
@@ -38,9 +38,90 @@ function verificarIdade() {
 
 function contarCaracteres() {
 
-    let texto1 = Text (document.getElementById("texto1").value);
-    let numeroCaracteres = Number (texto1.length);
+    let texto = String (document.getElementById("texto1").value);
+    let numeroCaracteres = texto.length;
 
-    document.getElementById("resultado3").innerText = "Número caracteres: " + (numeroCaracteres);
+    document.getElementById("resultado3").innerText = "Número caracteres: " + numeroCaracteres;
 
 }
+
+//EX 4 - VERIFICAR PAR IMPAR
+
+function verificarImpar() {
+
+    let numero = Number (document.getElementById("numero3").value);
+
+    if ( numero == 0 ) {
+
+        document.getElementById("resultado4").innerText = "Número é zero.";
+
+    } 
+    else if ( numero % 2 == 0 ) {
+        
+        document.getElementById("resultado4").innerText = "Número é par.";
+
+    } 
+    else {
+
+        document.getElementById("resultado4").innerText = "Número é impar.";
+
+    }
+
+}
+
+//Ex 5 - CAIXA ALTA!
+
+function transformarTextoCaixaAlta() {
+
+    let texto = String (document.getElementById("texto2").value);
+    let textoCaixaAlta = texto.toUpperCase();
+
+    document.getElementById("resultado5").innerText = "Seu texto em caixa-alta: " + textoCaixaAlta;
+
+}
+
+//EX 6 - MAIOR
+
+function verificarMaior() {
+
+    let numero1 = Number (document.getElementById("numero4").value);
+
+    let numero2 = Number (document.getElementById("numero5").value);
+
+    if ( numero1 > numero2 ) {
+        
+        document.getElementById("resultado6").innerText = numero1 + " é maior que " + numero2;
+
+    } 
+    else if ( numero1 < numero2 ) {
+
+        document.getElementById("resultado6").innerText = numero1 + " é menor que " + numero2;
+
+    }
+    else {
+
+        document.getElementById("resultado6").innerText = numero1 + " é igual á " + numero1;
+
+    }
+
+}
+
+//EX 7 - TABUADA
+
+function calcularTabuada() {
+
+    let numero = Number (document.getElementById("numero6").value);
+
+    document.getElementById("resultado7").innerText = 
+    numero + " * 1 = " + numero * 1 + "\n" +
+    numero + " * 2 = " + numero * 2 + "\n" +
+    numero + " * 3 = " + numero * 3 + "\n" +
+    numero + " * 4 = " + numero * 4 + "\n" +
+    numero + " * 5 = " + numero * 5 + "\n" +
+    numero + " * 6 = " + numero * 6 + "\n" +
+    numero + " * 7 = " + numero * 7 + "\n" +
+    numero + " * 8 = " + numero * 8 + "\n" +
+    numero + " * 9 = " + numero * 9 + "\n" +
+    numero + " * 10 = " + numero * 10;
+
+}s
